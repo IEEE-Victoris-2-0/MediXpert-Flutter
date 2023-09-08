21a0da8aaba1d49a3dca2a417d562ba05145e578
IbrahimHosny2002 <hosnyhema66@gmail.com>
2023-09-08 18:51:44 +0300

    added shared widgets

--- lib/core/shared/custombuttom.dart
+++ lib/core/shared/custombuttom.dart
@@ -1,17 +1,45 @@
-// ignore: depend_on_referenced_packages
 import 'package:flutter/material.dart';
 
 class Custombuttom extends StatelessWidget {
   final void Function()? onPressed;
-  final String? text;
+  final String buttontext;
 
-  const Custombuttom({required this.onPressed, required this.text, super.key});
-
+  const Custombuttom({
+    super.key, this.onPressed, required this.buttontext,});
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: Text("$text"),
+    return Container(
+      width: 131,
+      height: 37,
+      decoration: BoxDecoration(
+        gradient: const LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color(0xFF7642F9),
+            Color(0xFFC8B3FC),
+          ],
+        ),
+        borderRadius: BorderRadius.circular(23),
+      ),
+      child: ElevatedButton(
+        style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(23),
+              ),
+            ),
+            backgroundColor: MaterialStateProperty.all(Colors.transparent)),
+        onPressed: onPressed,
+        child: Text(
+          buttontext,
+          style: const TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w400,
+            color: Color(0xFFFFFFFF),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/core/shared/customfield.dart
+++ lib/core/shared/customfield.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
 
 class CustomField extends StatelessWidget {
-  const CustomField({super.key});
+  final String hinttext;
+  final IconData iconData;
+  final TextEditingController? mycontroller;
+  final String? Function(String?) valid;
+  final bool? obscureText;
+
+  const CustomField({
+    super.key,
+    required this.hinttext,
+    required this.iconData,
+    this.mycontroller,
+    required this.valid,
+    this.obscureText,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField();
+    return SizedBox(
+      width: 314,
+      height: 66,
+      child: TextFormField(
+        controller: mycontroller,
+        validator: valid,
+        obscureText: obscureText == null || obscureText == false ? false : true,
+        decoration: InputDecoration(
+            prefixIcon: Container(
+              padding:
+                  const EdgeInsets.only(left: 7, top: 7, bottom: 7, right: 30),
+              child: CircleAvatar(
+                radius: 26,
+                backgroundColor: Colors.white,
+                child: Icon(
+                  iconData,
+                  size: 30,
+                  color: const Color(0xFF7C47FC),
+                ),
+              ),
+            ),
+            filled: true,
+            fillColor: const Color(0xFFC8B3FC),
+            hintText: hinttext,
+            hintStyle: const TextStyle(
+              fontSize: 20,
+              color: Color(0xFFF6F2FF),
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(44),
+            )),
+      ),
+    );
   }
 }
