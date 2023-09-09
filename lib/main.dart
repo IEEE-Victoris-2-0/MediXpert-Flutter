46457931289f5156f774e67c26894266fe243e5f
IbrahimHosny2002 <hosnyhema66@gmail.com>
2023-09-10 00:18:09 +0300

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:tecdoc/view/screen/auth/signup.dart';
 import 'package:tecdoc/view/screen/onboarding.dart';
 
 Future<void> main() async {
@@ -18,6 +17,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
         debugShowCheckedModeBanner: false,
         title: "midxipert",
-        home: Signup());
+        home: OnBoardingScreen());
   }
 }
